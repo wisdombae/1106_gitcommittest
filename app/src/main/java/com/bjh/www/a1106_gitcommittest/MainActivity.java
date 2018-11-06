7610e9598152d9be37514f12a244883ec368ed0b
EDU <0narara0@gmail.com>
2018-11-06 13:28:26 +0900

    1

--- app/src/main/java/com/bjh/www/a1106_gitcommittest/MainActivity.java
+++ app/src/main/java/com/bjh/www/a1106_gitcommittest/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //34567890
+        //1111
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
